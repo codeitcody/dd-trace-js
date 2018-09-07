98831a4fcbd3af1c0f5b6948bdb692ac0f07fdda
Cody Schnacker <cody.schnacker@gmail.com>
2018-09-07 16:57:19 -0600

    Disable node version checking.

--- src/platform/node/load.js
+++ src/platform/node/load.js
@@ -5,17 +5,7 @@ const readPkgUp = require('read-pkg-up')
 const parentModule = require('parent-module')
 const semver = require('semver')
 
-const SUPPORTED_VERSIONS = '^4.7 || ^6.9 || ^6.11.2 || >=8'
-
 function load () {
-  if (!semver.satisfies(process.versions.node, SUPPORTED_VERSIONS)) {
-    throw new Error([
-      `Node ${process.versions.node} is not supported.`,
-      `Only versions of Node matching "${SUPPORTED_VERSIONS}" are supported.`,
-      `Tracing has been disabled.`
-    ].join(' '))
-  }
-
   const callerPath = parentModule()
   const parentPath = parentModule(callerPath)
   const cwd = path.dirname(parentPath || callerPath)
